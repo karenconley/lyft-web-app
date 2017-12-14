e3b3d24baaf678b3c0b8a28efad95193ec4325b7
karenconley <karen.c.echeverria@gmail.com>
2017-12-14 04:31:41 -0300

    subiendo Lyft-2

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+//.js Lyft
+/*Inicializando*/
+$(document).ready(function(){
+/*splash*/
+	setTimeout(function() {
+		$(".content").fadeOut(800);
+		},3000);
+		setTimeout(function() {
+			$(".content2").fadeIn(1000);
+		});
+});
