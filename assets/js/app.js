29d8598b679c632678a202dc86f46d883eda85d0
karenconley <karen.c.echeverria@gmail.com>
2018-01-02 23:19:11 -0300

    modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,52 @@
 $(document).ready(function(){
 
-/*Splash*/
+/* Splash */
 	setTimeout(function() {
-		$(".content").fadeOut(800);
+		$('.intro').fadeOut(800);
 		},2500);
 		setTimeout(function() {
-			$(".content2").fadeIn(1000);
+			$('.home').fadeIn(1000);
 		});
 });
+
+
+/*Botón y caracteres numéricos*/
+$('#phoneNumber').keyup(function(){
+	if($(this).val().length === 10){
+		$('#btnCode').removeClass('disabled');
+	}if($(this).val().length < 10){
+		$('#btnCode').addClass('disabled');
+	}if($(this).val().length > 10){
+		$('#btnCode').addClass('disabled');
+		$('#phoneNumber').prop('disabled',true); /*Desabilitar*/
+		$('#phoneNumber').prop('disabled',false); /*Activar*/
+	}if (isNaN($(this).val())){
+		$('#contNumber').text('Only Number');
+		$('#btnCode').addClass('disabled');
+	}else{
+		$('#contNumber').text('');
+	}
+	return false;
+});
+
+
+/*Código*/
+$('#codeButton').click(code);
+function code() {
+	var code = '';
+	var str = ('123456789');
+	for (var i = 0; i < 3; i++) {
+		code += str.charAt(Math.floor(Math.random() * str.length));
+	}
+	alert('Tu código: LAB-' + code);
+}
+
+$('#verifyButton').click(final);
+function final() {
+	$('.signUp').show();
+	$('.home').hide();
+}
+
+$('#modal1').on('shown.bs.modal', function(){
+	$('#myInput').focus()
+})
