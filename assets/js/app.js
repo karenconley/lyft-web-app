7a8229215423b365562e7d487bb2115c2d9b4e86
karenconley <karen.c.echeverria@gmail.com>
2018-01-02 23:50:15 -0300

    aparece modal predeterminado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,52 +1,99 @@
 $(document).ready(function(){
 
 /* Splash */
-	setTimeout(function() {
-		$('.intro').fadeOut(800);
-		},2500);
-		setTimeout(function() {
-			$('.home').fadeIn(1000);
-		});
+    setTimeout(function() {
+        $('.intro').fadeOut(800);
+        },2500);
+        setTimeout(function() {
+            $('.home').fadeIn(1000);
+        });
 });
 
+/**/
 
-/*Botón y caracteres numéricos*/
-$('#phoneNumber').keyup(function(){
-	if($(this).val().length === 10){
-		$('#btnCode').removeClass('disabled');
-	}if($(this).val().length < 10){
-		$('#btnCode').addClass('disabled');
-	}if($(this).val().length > 10){
-		$('#btnCode').addClass('disabled');
-		$('#phoneNumber').prop('disabled',true); /*Desabilitar*/
-		$('#phoneNumber').prop('disabled',false); /*Activar*/
-	}if (isNaN($(this).val())){
-		$('#contNumber').text('Only Number');
-		$('#btnCode').addClass('disabled');
-	}else{
-		$('#contNumber').text('');
-	}
-	return false;
+$(function() {
+    
+    var $formLogin = $('#login-form');
+    var $formLost = $('#lost-form');
+    var $formRegister = $('#register-form');
+    var $divForms = $('#div-forms');
+    var $modalAnimateTime = 300;
+    var $msgAnimateTime = 150;
+    var $msgShowTime = 2000;
+
+    $("form").submit(function () {
+        switch(this.id) {
+            case "login-form":
+                var $lg_username=$('#login_username').val();
+                var $lg_password=$('#login_password').val();
+                if ($lg_username == "ERROR") {
+                    msgChange($('#div-login-msg'), $('#icon-login-msg'), $('#text-login-msg'), "error", "glyphicon-remove", "Login error");
+                } else {
+                    msgChange($('#div-login-msg'), $('#icon-login-msg'), $('#text-login-msg'), "success", "glyphicon-ok", "Login OK");
+                }
+                return false;
+                break;
+            case "lost-form":
+                var $ls_email=$('#lost_email').val();
+                if ($ls_email == "ERROR") {
+                    msgChange($('#div-lost-msg'), $('#icon-lost-msg'), $('#text-lost-msg'), "error", "glyphicon-remove", "Send error");
+                } else {
+                    msgChange($('#div-lost-msg'), $('#icon-lost-msg'), $('#text-lost-msg'), "success", "glyphicon-ok", "Send OK");
+                }
+                return false;
+                break;
+            case "register-form":
+                var $rg_username=$('#register_username').val();
+                var $rg_email=$('#register_email').val();
+                var $rg_password=$('#register_password').val();
+                if ($rg_username == "ERROR") {
+                    msgChange($('#div-register-msg'), $('#icon-register-msg'), $('#text-register-msg'), "error", "glyphicon-remove", "Register error");
+                } else {
+                    msgChange($('#div-register-msg'), $('#icon-register-msg'), $('#text-register-msg'), "success", "glyphicon-ok", "Register OK");
+                }
+                return false;
+                break;
+            default:
+                return false;
+        }
+        return false;
+    });
+    
+    $('#login_register_btn').click( function () { modalAnimate($formLogin, $formRegister) });
+    $('#register_login_btn').click( function () { modalAnimate($formRegister, $formLogin); });
+    $('#login_lost_btn').click( function () { modalAnimate($formLogin, $formLost); });
+    $('#lost_login_btn').click( function () { modalAnimate($formLost, $formLogin); });
+    $('#lost_register_btn').click( function () { modalAnimate($formLost, $formRegister); });
+    $('#register_lost_btn').click( function () { modalAnimate($formRegister, $formLost); });
+    
+    function modalAnimate ($oldForm, $newForm) {
+        var $oldH = $oldForm.height();
+        var $newH = $newForm.height();
+        $divForms.css("height",$oldH);
+        $oldForm.fadeToggle($modalAnimateTime, function(){
+            $divForms.animate({height: $newH}, $modalAnimateTime, function(){
+                $newForm.fadeToggle($modalAnimateTime);
+            });
+        });
+    }
+    
+    function msgFade ($msgId, $msgText) {
+        $msgId.fadeOut($msgAnimateTime, function() {
+            $(this).text($msgText).fadeIn($msgAnimateTime);
+        });
+    }
+    
+    function msgChange($divTag, $iconTag, $textTag, $divClass, $iconClass, $msgText) {
+        var $msgOld = $divTag.text();
+        msgFade($textTag, $msgText);
+        $divTag.addClass($divClass);
+        $iconTag.removeClass("glyphicon-chevron-right");
+        $iconTag.addClass($iconClass + " " + $divClass);
+        setTimeout(function() {
+            msgFade($textTag, $msgOld);
+            $divTag.removeClass($divClass);
+            $iconTag.addClass("glyphicon-chevron-right");
+            $iconTag.removeClass($iconClass + " " + $divClass);
+  		}, $msgShowTime);
+    }
 });
-
-
-/*Código*/
-$('#codeButton').click(code);
-function code() {
-	var code = '';
-	var str = ('123456789');
-	for (var i = 0; i < 3; i++) {
-		code += str.charAt(Math.floor(Math.random() * str.length));
-	}
-	alert('Tu código: LAB-' + code);
-}
-
-$('#verifyButton').click(final);
-function final() {
-	$('.signUp').show();
-	$('.home').hide();
-}
-
-$('#modal1').on('shown.bs.modal', function(){
-	$('#myInput').focus()
-})
